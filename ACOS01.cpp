219f8954d0bc2059d2211d1ee4b64d44742110ae
anaoliveira04 <124443707+anaoliveira04@users.noreply.github.com>
2023-10-24 15:52:23 -0300

    Add files via upload

---
+++ ACOS01.cpp
@@ -0,0 +1,16 @@
+//AOS01 - cartão de visitas 
+//Ana Clara Oliveira Silva 03/03/2023
+
+#include <stdio.h>
+
+main ()
+{
+	printf ("+-----------------------------+\n");
+	printf ("| Ana Clara Oliveira Silva    |\n");
+	printf ("| R. Marques de Paranagua, 111|\n");
+	printf ("| Sao Paulo, SP               |\n");
+	printf ("| (11) 988786266              |\n");
+	printf ("+-----------------------------+\n"); 
+	
+
+ } 
